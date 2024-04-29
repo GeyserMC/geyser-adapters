2a3d3943772b798ed4a7e0d0a6c815f74b20b1ce
chris <github@onechris.mozmail.com>
2024-04-29 10:36:10 +0200

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.papermc.paperweight:paperweight-userdev:1.6.3-SNAPSHOT")
+    implementation("io.papermc.paperweight:paperweight-userdev:1.6.2")
     implementation("com.github.johnrengelman", "shadow", "8.1.1")
 }
 
