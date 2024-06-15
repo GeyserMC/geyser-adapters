28b872e58cc256fd928a3a9aa17e166af2dc69e1
chris <github@onechris.mozmail.com>
2024-06-15 18:25:22 +0200

    1.21 Spigot adapters (#7)
    * 1.21 Spigot adapters
    
    * Bump adapter version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("io.papermc.paperweight:paperweight-userdev:1.6.2")
+    implementation("io.papermc.paperweight:paperweight-userdev:1.7.1")
     implementation("com.github.johnrengelman", "shadow", "8.1.1")
 }
 
---
+++ spigot/v1_21_R1/build.gradle.kts
@@ -0,0 +1,17 @@
+
+plugins {
+    id("adapters.spigot.paperweight-conventions")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    implementation(projects.spigot.base)
+    paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
+}
